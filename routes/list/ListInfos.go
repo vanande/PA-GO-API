dac6e479719d10625b23df27fb4616561e1be56d
Debian <debian@vps-caf35801.vps.ovh.net>
2023-05-09 16:55:49 +0000

    Add comment about magic part

--- routes/list/ListInfos.go
+++ routes/list/ListInfos.go
@@ -21,35 +21,44 @@ func ListInfos(w http.ResponseWriter, req *http.Request) {
 		}
 		defer selectQuery.Close()
 
-		rows := []map[string]interface{}{}
+		// Magic start here
+		var dataRows []map[string]interface{} // Create a slice of hashmap to store the data
 		for selectQuery.Next() {
-			columns, _ := selectQuery.Columns()
-			values := make([]interface{}, len(columns))
-			scanArgs := make([]interface{}, len(values))
-			for i := range values {
-				scanArgs[i] = &values[i]
+			columnNames, _ := selectQuery.Columns()                 // Get the column names to init the map
+			columnValues := make([]interface{}, len(columnNames))   // Create a slice(= improved array -> very opti) of interface{} to store the values
+			valuePointers := make([]interface{}, len(columnValues)) // Create a slice of interface{} to store the pointers of the multiple &var, &var2, &var3, etc.
+			for i := range columnValues {
+				valuePointers[i] = &columnValues[i] // Store the pointers of the multiple &var, &var2, &var3, etc. in the slice
 			}
-			err = selectQuery.Scan(scanArgs...)
+			err = selectQuery.Scan(valuePointers...) // Scan need addresses of the variables to store the values
 			if err != nil {
 				fmt.Println(err)
 				continue
 			}
+			// INIT DONE
+			// Now we have the column names and the values stored in the slice columnValues
+			// We can now create a map with the column names as keys and the values as values like this:
+			// columnNames = ["id", "name", "description"]
+			// columnValues = ["1", "Super duper test", "Super description"]
 
-			row := make(map[string]interface{})
-			for i, col := range values {
-				if col != nil {
-					switch v := col.(type) {
+			// This part is just to create the map with the column names as keys and the values as values
+			// dataRow = {"id": "1", "name": "Super duper test", "description": "Super description"}
+			dataRow := make(map[string]interface{})
+			for i, val := range columnValues {
+				if val != nil {
+					switch v := val.(type) {
 					case []byte:
-						row[columns[i]] = string(v)
+						dataRow[columnNames[i]] = string(v) // Small hacks to avoid strange values like é$ù&é"àç_è-ç�
 					default:
-						row[columns[i]] = v
+						dataRow[columnNames[i]] = v // Example : dataRow["id"] = "1"
 					}
 				}
 			}
-			rows = append(rows, row)
+			dataRows = append(dataRows, dataRow) // Obviously append the dataRow to the slice of map
+			// dataRows = [{"id": "1", "name": "Super duper test", "description": "Super description"}]
 		}
 
-		if len(rows) == 0 {
+		if len(dataRows) == 0 {
 			libraries.Response(w, map[string]interface{}{
 				"message": "Invalid data",
 			}, http.StatusNotFound)
@@ -58,7 +67,7 @@ func ListInfos(w http.ResponseWriter, req *http.Request) {
 
 		libraries.Response(w, map[string]interface{}{
 			"message": "Successfully fetched data",
-			"data":    rows,
+			"data":    dataRows,
 		}, http.StatusOK)
 	}
 }
