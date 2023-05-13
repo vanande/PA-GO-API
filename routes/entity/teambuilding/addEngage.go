f9a9e362c5bf9ab93b8beb5fc14ff904cc8bf1da
vanande <101594646+vanande@users.noreply.github.com>
2023-05-13 08:44:34 +0100

    add engage crd in tb

---
+++ routes/entity/teambuilding/addEngage.go
@@ -0,0 +1,34 @@
+package teambuilding
+
+import (
+	"TogetherAndStronger/libraries"
+	"TogetherAndStronger/routes/db/query"
+	"fmt"
+	"net/http"
+)
+
+func AddEngage(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "POST":
+		data := libraries.Body(w, req)
+
+		//											-->	change here <--
+		_, err := query.InsertQuery("engage", map[string]interface{}{
+			// Data like that
+			"idTEAM_BUILDING": data["idt"],
+			"idPrestataire":   data["idp"],
+			"prix_a_payer":    data["prix"],
+		})
+		if err != nil {
+			fmt.Println(err)
+			libraries.Response(w, map[string]interface{}{
+				"message": "Bad request",
+			}, http.StatusBadRequest)
+			return
+		}
+
+		libraries.Response(w, map[string]interface{}{
+			"message": "Successfully added",
+		}, http.StatusOK)
+	}
+}
---
+++ routes/entity/teambuilding/getPresta.go
@@ -0,0 +1,83 @@
+package teambuilding
+
+import (
+	"TogetherAndStronger/libraries"
+	"TogetherAndStronger/routes/db/query"
+	"fmt"
+	"net/http"
+)
+
+type Presta struct {
+	IdPrestataire string `json:"id"`
+	Nom           string `json:"nom"`
+	Prenom        string `json:"prenom"`
+	Tel           string `json:"tel"`
+	Email         string `json:"email"`
+	Metier        string `json:"metier"`
+	Description   string `json:"description"`
+	NomEntreprise string `json:"nomEntreprise"`
+	Rib           string `json:"rib"`
+	Valide        string `json:"valide"`
+	IdAdresse     string `json:"idAdresse"`
+	Image         string `json:"image"`
+	Prix          string `json:"prix"`
+}
+
+func GetPresta(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "POST":
+		data := libraries.Body(w, req)
+
+		id, OK := data["id"].(string)
+		if !OK {
+			libraries.Response(w, map[string]interface{}{
+				"message": "Invalid parameters",
+			}, http.StatusBadRequest)
+			return
+		}
+
+		// tables in the right order
+		tables := []string{"engage e", "prestataire p"}
+		// columns to look for
+		columns := []string{"p.idPRESTATAIRE", "p.nom", "p.prenom", "p.tel", "p.email", "p.metier", "p.description", "p.nom_entreprise", "p.rib", "p.valide", "p.idADRESSE", "p.image", "e.prix_a_payer"}
+		// on what to join
+		joins := []map[string]string{
+			{"e.idPRESTATAIRE": "p.idPRESTATAIRE"},
+		}
+		// the where at the end
+		conditions := map[string]interface{}{
+			"idTEAM_BUILDING": id,
+		}
+
+		rows, err := query.SelectWithInnerJoin(tables, columns, joins, conditions)
+		if err != nil {
+			fmt.Println(err)
+			libraries.Response(w, map[string]interface{}{
+				"message": "No data found",
+			}, http.StatusNotFound)
+			return
+		}
+
+		var res []Presta
+
+		for rows.Next() {
+			var p Presta
+			// let copilot do the job -> ctrl+x (the line under 'err := ...') -> wait a bit -> tab
+			err := rows.Scan(&p.IdPrestataire, &p.Nom, &p.Prenom, &p.Tel, &p.Email, &p.Metier, &p.Description, &p.NomEntreprise, &p.Rib, &p.Valide, &p.IdAdresse, &p.Image, &p.Prix)
+			if err != nil {
+				fmt.Println(err)
+			}
+			res = append(res, p)
+		}
+
+		libraries.Response(w, map[string]interface{}{
+			"message": "Successfully fetched data",
+			"data":    res,
+		}, http.StatusOK)
+
+	default:
+		libraries.Response(w, map[string]interface{}{
+			"message": "Method not allowed",
+		}, http.StatusMethodNotAllowed)
+	}
+}
